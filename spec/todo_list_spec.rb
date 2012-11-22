59575f2935c51ae9b49eefc04737cd636b2df4cd
Mike Mitrovich <mmitrovich@gmail.com>
2012-11-21 22:47:52 -0800

    Initial commit

---
+++ spec/todo_list_spec.rb
@@ -0,0 +1,14 @@
+require_relative '../lib/todo_list'
+
+describe ToDoList do
+	
+	it "starts with an empty list"
+
+	context "with one todo added" do
+		it "has a size of 1"
+	end
+
+	context "with two todos added" do
+		it "has a size of 2"
+	end
+end
